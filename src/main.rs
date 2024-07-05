f9eeccf09b83d5fe8a56ce64068aad307bdfdcc6
Vivian Morton <vivian.morton404@gmail.com>
2024-07-05 16:33:43 -0400

    Can now guess a number and will be told if you win or lose

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io; //User input library
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     
-    println!("Please guess a number");
-
     let secret_number = rand::thread_rng().gen_range(1..=100); //thread_rng runs on this thread, range 1 to 100 inclusive
+
+    println!("Please guess a number");
 
     let mut guess = String::new(); //mutable empty string so we can change it later
 
@@ -15,7 +16,17 @@ fn main() {
 
     //io::stdin().read_line(&mut guess).expect("Failed to read line"); an alternative to the above line
 
-    print!("\nYou guessed: {guess}"); //Print guess
-    print!("The secret number is: {secret_number}\n");
+    let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+
+
+    println!("\nYou guessed: {guess}"); //Print guess
+    println!("The secret number is: {secret_number}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("You win!"),
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+    };
+
 
 }
